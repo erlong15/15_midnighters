0016746b7c28535e71e3e5ef9eedfd6c159ae1b0
Alex <lucky_is@mail.ru>
2017-10-10 18:49:17 +0300

    version 1

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+Check devman API for records about solution's attempts which were sent after midnight. 
+
+# How to use 
+Fo running you need a python 3.4 and upper
+
+You have to install requirements
+
+```
+pip install -r requrements.txt
+```
+
+After that you can run the script
+
+```
+python3 seek_dev_nighters.py 
+User tatarstroy sent a night solution last time at 04-10-2017 03-21-12 MSK
+User mmatveev sent a night solution last time at 04-10-2017 00-48-44 MSK
+User RamusikValery sent a night solution last time at 08-10-2017 03-50-50 MSK
+```
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,60 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pytz
+from datetime import datetime
 
-def get_midnighters():
-    pass
+
+def get_attempt_time(attempt_record):
+    student_tz = pytz.timezone(attempt_record['timezone'])
+    utc_dt = datetime.utcfromtimestamp(attempt_record['timestamp'])
+    return student_tz.localize(utc_dt)
+
+
+def get_page_with_attempts(api_url, payload):
+    response = requests.get(api_url, payload)
+    return response.json()
+
+
+def get_attempts_and_next_page(api_url, page):
+    attempts = get_page_with_attempts(api_url, {'page': page})
+    is_next_page = attempts['page'] < attempts['number_of_pages']
+    return is_next_page, attempts['records']
+
+
+def load_attempts(api_url):
+    page = 1
+    is_next_page = True
+
+    while is_next_page:
+        is_next_page, attempt_records = get_attempts_and_next_page(api_url,
+                                                                   page)
+        page += 1
+        for record in attempt_records:
+            if record['timestamp']:
+                yield record
+
+
+def get_midnighters(attempt_records):
+    midnighters = {}
+    min_hour = 0
+    max_hour = 5
+
+    for record in attempt_records:
+        attempt_time = get_attempt_time(record)
+        if attempt_time.hour in range(min_hour, max_hour):
+            midnighters.setdefault(record['username'],
+                                   []).append(attempt_time)
+    return midnighters
+
+
+def main():
+    api_url = 'http://devman.org/api/challenges/solution_attempts/'
+    output = "User {user} sent a night solution " \
+             "last time at {last_time}"
+    for user, dates in get_midnighters(load_attempts(api_url)).items():
+        last_time = max(dates).strftime('%d-%m-%Y %H-%M-%S %Z')
+        fmt_params = {'user': user, 'last_time': last_time}
+        print(output.format(**fmt_params))
+
 
 if __name__ == '__main__':
-  pass
+    main()
